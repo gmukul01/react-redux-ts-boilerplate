7a6b718d00b78bace4ae4e18c5bf794617b5834c
gmukul01 <mukulbansal01@gmail.com>
2019-03-12 00:00:02 +0700

    Added styled component linting and dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { default as styled } from 'styled-components';
+
+const Text = styled.p`
+    color: black;
+`;
 
 const App = () => {
     return (
         <div>
-            <p>First Commit</p>
+            <Text>First Commit</Text>
         </div>
     );
 };
