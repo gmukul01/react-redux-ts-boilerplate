f27dfa559bad310851578b0a4dff8c9761d594fc
Mukul Bansal <mukul@Mukuls-MacBook-Pro.local>
2019-04-04 17:14:36 +0530

    Fixed jest path issue

--- config/jest/jest.config.js
+++ config/jest/jest.config.js
@@ -1,11 +1,26 @@
 module.exports = {
-    collectCoverageFrom: ['../../src/**/*.(ts|tsx)', '!../../src/*.(ts|tsx)', '!../../src/theme/**', '!../../node_modules/**'],
-    coverageDirectory: '../../coverage/',
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.(ts|tsx)',
+        '!<rootDir>/src/*.(ts|tsx)',
+        '!<rootDir>/src/theme/**',
+        '!<rootDir>/node_modules/**'
+    ],
+    coverageDirectory: '<rootDir>/coverage/',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    roots: ['../../src/'],
-    setupFilesAfterEnv: ['./enzymeSetup.js'],
+    roots: ['<rootDir>/src/'],
+    rootDir: '../../',
+    setupFilesAfterEnv: ['<rootDir>/config/jest/enzymeSetup.js'],
     snapshotSerializers: ['enzyme-to-json/serializer'],
     testEnvironment: 'jsdom',
     testMatch: ['**/*.(spec|test).(ts|tsx)'],
-    verbose: true
+    verbose: true,
+    moduleNameMapper: {
+        '^@components': '<rootDir>/src/components',
+        '^@components/(.*)': '<rootDir>/src/components/$1',
+        '^@store': '<rootDir>/src/store',
+        '^@store/(.*)': '<rootDir>/src/store/$1',
+        '^@theme': '<rootDir>/src/theme/index.ts',
+        '^@theme/(.*)': '<rootDir>/src/theme/$1',
+        '^@utils/(.*)': '<rootDir>/src/utils/$1'
+    }
 };
