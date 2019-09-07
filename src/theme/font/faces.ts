8cd7d661b8bc22d911e035c7cbac358c76483f6d
gmukul01 <mukulbansal01@gmail.com>
2019-09-07 17:08:12 +0530

    Updated font and text theme

--- src/theme/font/faces.ts
+++ src/theme/font/faces.ts
@@ -1,22 +1,23 @@
+// tslint:disable: max-line-length
 import { FontFaceTheme } from './types';
 
-const appleSdGothicNeo: FontFaceTheme = {
-    fontFamily: 'apple SD gothic neo',
+const proximaNova: FontFaceTheme = {
+    fontFamily: 'ProximaNova',
     fontFaces: [
         {
-            primarySrc: `local('Apple SD Gothic Neo Light')`,
+            primarySrc: `url('https://d1b6bucc9jhzue.cloudfront.net/at/fonts/ProximaNova/webfonts/ProximaNovaLight.eot?cloudFrontFixScheme=http&cloudFrontFixHost=www2.vauto.com') format('eot')`,
             secondarySrc: '',
             fontWeight: 200,
             fontStyle: 'normal'
         },
         {
-            primarySrc: `local('Apple SD Gothic Neo Light')`,
+            primarySrc: `url('https://d1b6bucc9jhzue.cloudfront.net/at/fonts/ProximaNova/webfonts/ProximaNovaRegular.eot?cloudFrontFixScheme=http&cloudFrontFixHost=www2.vauto.com') format('eot')`,
             secondarySrc: '',
             fontWeight: 400,
             fontStyle: 'normal'
         },
         {
-            primarySrc: `local('Apple SD Gothic Neo Bold')`,
+            primarySrc: `url('https://d1b6bucc9jhzue.cloudfront.net/at/fonts/ProximaNova/webfonts/ProximaNovaBold.eot?cloudFrontFixScheme=http&cloudFrontFixHost=www2.vauto.com') format('eot')`,
             secondarySrc: '',
             fontWeight: 700,
             fontStyle: 'normal'
@@ -24,6 +25,6 @@ const appleSdGothicNeo: FontFaceTheme = {
     ]
 };
 
-const faces: FontFaceTheme[] = [appleSdGothicNeo];
+const faces: FontFaceTheme[] = [proximaNova];
 
 export default faces;
